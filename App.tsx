0bac6398afaa5134f842f59c82a5ae9420df840b
Leandro Ramos <leanduardo@hotmail.com>
2021-11-22 12:12:01 -0300

    feat: Ajustes no cofinancés

--- App.tsx
+++ App.tsx
@@ -11,6 +11,8 @@ import {
 import theme from './src/global/styles/theme'
 
 import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +27,8 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <Register />
     </ThemeProvider>
   );
 }
